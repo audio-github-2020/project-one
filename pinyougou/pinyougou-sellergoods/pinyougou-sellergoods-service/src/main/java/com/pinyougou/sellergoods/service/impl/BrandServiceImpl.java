adb4804d15e29104023117956733fda6bb27f069
arvin <mingtian.bao@foxmail.com>
2020-02-06 16:55:39 +0800

    初始化搭建品优购工程
    1.工程搭建
    2.实现品牌列表数据加载，并完成测试

---
+++ pinyougou/pinyougou-sellergoods/pinyougou-sellergoods-service/src/main/java/com/pinyougou/sellergoods/service/impl/BrandServiceImpl.java
@@ -0,0 +1,28 @@
+package com.pinyougou.sellergoods.service.impl;
+
+import com.alibaba.dubbo.config.annotation.Service;
+import com.pinyougou.mapper.BrandMapper;
+import com.pinyougou.pojo.TbBrand;
+import com.pinyougou.sellergoods.service.BrandService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+@Service(interfaceClass = BrandService.class)
+public class BrandServiceImpl implements BrandService {
+
+    @Autowired
+    private BrandMapper brandMapper;
+
+    @Override
+    public List<TbBrand> queryAll() {
+//        System.out.println("123");
+//        System.out.println("123");
+//        System.out.println("123");
+//        System.out.println("123");
+//        System.out.println("123");
+//        System.out.println("123");
+//        System.out.println("123");
+        return brandMapper.queryAll();
+    }
+}
